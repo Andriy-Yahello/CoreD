ad9908a529e045ca8a9fbf6f6f8b2ea83c68e2c1
Andriy-Yahello <andrey.lv.west@gmail.com>
2017-05-08 13:30:26 -0700

    Using CoreData
    Here you can manualy send user input in core data. All output is shown
    in console output.

--- CoreD/ViewController.swift
+++ CoreD/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    var usernameArr:[String] = []
+    var passwordArr:[String] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Storing data
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        //insert data
+        /*let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("Ben", forKey: "username")
+            newUser.setValue("667", forKey: "password")
+        
+        do{
+        try context.save()
+            print("Saved")
+        }catch{
+            print("error")
+        }*/
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        do
+        {
+        let results = try context.fetch(request)
+            
+            if results.count > 0
+        
+        {
+            for result in results as! [NSManagedObject]
+            {
+                if let username = result.value(forKey: "username") as? String
+                {
+                    //print(username)
+                    usernameArr.append(username)
+                }
+                if let password = result.value(forKey: "password") as? String
+                {
+                    //print(password)
+                    passwordArr.append(password)
+                }
+            }
+            print (usernameArr)
+            print (passwordArr)
+        }
+        }
+    catch
+    {
+    print("error")
+    }
     }
 
     override func didReceiveMemoryWarning() {
